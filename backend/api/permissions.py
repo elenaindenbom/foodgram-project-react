34ac27c8c9f3543ee506d37d47336f02d5fe566c
Åëåíà Èíäåíáîì <elenaindenbom@gmail.com>
2023-02-12 20:20:40 +0300

    Внесены исправления

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,11 +2,13 @@ from rest_framework import permissions
 
 
 class AuthorAdminOrReadOnly(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
             or request.user.is_authenticated
-            and request.user.is_staff
         )
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_staff)
--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,4 +1,4 @@
-from csv import DictReader
+import csv
 
 from django.core.management import BaseCommand
 from recipes.models import Ingredient
@@ -8,21 +8,12 @@ class Command(BaseCommand):
     help = 'Загрузка из csv файла'
 
     def handle(self, *args, **kwargs):
-        if Ingredient.objects.exists():
-            print('В базе уже есть данные.')
-            return
-        try:
-            with open(
-                './data/ingredients.csv',
-                'r',
-                encoding='utf-8'
-            ) as file:
-                reader = DictReader(file)
-                Ingredient.objects.bulk_create(
-                    Ingredient(**data) for data in reader)
-        except ValueError:
-            print('Неопределенное значение.')
-        except Exception:
-            print('Что-то пошло не так!')
-        else:
-            print('Загрузка окончена.')
+        with open('ingredients.csv', 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            Ingredient.objects.bulk_create(
+                [Ingredient(
+                    name=row[0],
+                    measurement_unit=row[1]
+                ) for row in reader]
+            )
+        print('Загрузка завершена.')
